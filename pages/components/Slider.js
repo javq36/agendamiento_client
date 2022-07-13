a8add773b1ce47dd77a8906ca589260fb7334921
Alexander Villamizar <33869219+javq36@users.noreply.github.com>
2022-07-13 18:56:12 -0500

    solved issues with carrousel

--- pages/components/Slider.js
+++ pages/components/Slider.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import Image from 'next/image'
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+import cat from "../../assets/gato.jpg";
 
 export function Slider() {
-    return (
-        <Carousel>
-            <div>
-                <Image
-                    src="/gato.jpg"
-                    alt="Picture of the author"
-                    width={500}
-                    height={200}
-                />
-            </div>
-        </Carousel>
-    );
+  return (
+    <Carousel autoPlay infiniteLoop interval={3000} >
+      <div key={1}>
+        <Image src={cat} width={600} height={250} style={{}}/>
+      </div>
+      <div key={2}>
+        <Image src={cat} width={600} height={250} style={{}}/>
+      </div>
+      <div key={3}>
+        <Image src={cat} width={600} height={250} style={{}}/>
+      </div>
+      <div key={4}>
+        <Image src={cat} width={600} height={250} style={{}}/>
+      </div>
+    </Carousel>
+  );
 }
