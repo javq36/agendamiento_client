b6a8be7d77ca1f292b5b419e9137e3e6e0002e05
RVSebastian <108822407+RVSebastian@users.noreply.github.com>
2022-07-26 08:21:23 -0500

    fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6dabab1ac68657d"; }
+/******/ 		__webpack_require__.h = function() { return "dc68639b59ef14dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
